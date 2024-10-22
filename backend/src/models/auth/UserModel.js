652b22e2f582c34cb3b6a3513ff072c2a81abbb8
antonclayton <anton.clayton@gmail.com>
2024-10-21 19:38:13 -0700

    added minimum password length to UserSchema

--- backend/src/models/auth/UserModel.js
+++ backend/src/models/auth/UserModel.js
@@ -17,6 +17,7 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, "Please provide password"],
+        minLength: [6, "Password must be at least 6 characters long"],
     },
     photo: {
         type: String,
